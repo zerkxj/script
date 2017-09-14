cd08d336d931948eb60f2896dc6a11de3691e2cc
zerkxj <zerkxj@gmail.com>
2017-09-14 15:55:36 +0800

    description; first commit
    modify file:
      1. command_alias.sh
         git_init_setup.sh
         merge_branch.sh
        1. new added

---
+++ git/git_init_setup.sh
@@ -0,0 +1,11 @@
+#/bin/bash
+#-------------------------------------------------------------------------------
+#  initil setup GIT
+#    1. user name 
+#    2. e-mail
+#    3. editor
+#-------------------------------------------------------------------------------
+git config --global user.name "zerkxj"
+git config --global user.email zerkxj@gmail.com
+git config --global core.editor vim
+git config --global push.default simple
